1d22645234ea89ca5d73ad22914b93525a8058ce
Dan McElroy <dan.mcelroy@huuugegames.com>
2020-06-25 11:35:43 +0100

    Horizontal scroll list of exercise images now populated on info page.

--- app/src/main/java/com/gymondo/xrciser/activities/ExerciseInfoActivity.kt
+++ app/src/main/java/com/gymondo/xrciser/activities/ExerciseInfoActivity.kt
@@ -1,13 +1,19 @@
 package com.gymondo.xrciser.activities
 
 import android.os.Bundle
+import android.util.AttributeSet
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.gymondo.xrciser.R
+import com.gymondo.xrciser.client.ImageClient
+import com.gymondo.xrciser.data.ExerciseImage
 import com.gymondo.xrciser.data.ExerciseInfo
 import com.gymondo.xrciser.services.ExerciseService
+import com.squareup.picasso.Picasso
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
@@ -15,10 +21,12 @@ const val EXERCISE_ID = "com.gymondo.xrciser.EXERCISE_ID"
 
 class ExerciseInfoActivity : AppCompatActivity() {
 
-    lateinit var exerciseInfo : ExerciseInfo
+    private var exerciseId: Int = -1
+    private lateinit var exerciseInfo : ExerciseInfo
 
-    lateinit var exerciseName: TextView
-    lateinit var categoryName: TextView
+    private lateinit var exerciseName: TextView
+    private lateinit var categoryName: TextView
+    private lateinit var imageList: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +35,7 @@ class ExerciseInfoActivity : AppCompatActivity() {
 
         exerciseName = findViewById(R.id.exercise_name)
         categoryName = findViewById(R.id.category_name)
+        imageList = findViewById(R.id.image_list)
 
         loadExercise()
 
@@ -37,7 +46,7 @@ class ExerciseInfoActivity : AppCompatActivity() {
     }
 
     private fun loadExercise() {
-        val exerciseId = intent.getIntExtra(EXERCISE_ID, -1)
+        exerciseId = intent.getIntExtra(EXERCISE_ID, -1)
 
         ExerciseService.create().getExerciseInfo(exerciseId)
             .subscribeOn(Schedulers.io())
@@ -52,5 +61,34 @@ class ExerciseInfoActivity : AppCompatActivity() {
     private fun renderExercise() {
         exerciseName.text = exerciseInfo.name
         categoryName.text = exerciseInfo.category.name
+
+        loadImages()
+    }
+
+    private fun loadImages() {
+        val imageRequest = ImageClient.getImagesForExercise(exerciseId)
+        imageRequest.subscribe { images ->
+            if (images.isEmpty()) {
+                addImageView()
+                return@subscribe
+            }
+            for (image in images) {
+                addImageView(image)
+            }
+        }
+    }
+
+    private fun addImageView(image : ExerciseImage? = null ) {
+        val imageView = ImageView(this)
+        imageList.addView(imageView)
+        if (image is ExerciseImage) {
+            Picasso.get().load(image.url)
+                .placeholder(R.drawable.exercise)
+                .error(R.drawable.exercise)
+                .into(imageView)
+            imageView.contentDescription = exerciseInfo.name
+        } else {
+            Picasso.get().load(R.drawable.exercise).into(imageView)
+        }
     }
 }
